f4d6c7ed25d339a926b3e57edb6f2eb701c8dc67
Banjo Babade <rulesxx@gmail.com>
2025-05-30 18:28:05 +0100

    Updated with naming conventions

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "aws_security_group" "banjo_cluster_sg" {
   }
 
   tags = {
-    Name = "devopsshack-cluster-sg"
+    Name = "banjo-cluster-sg"
   }
 }
 
@@ -82,25 +82,25 @@ resource "aws_security_group" "banjo_node_sg" {
   }
 
   tags = {
-    Name = "devopsshack-node-sg"
+    Name = "banjo-node-sg"
   }
 }
 
-resource "aws_eks_cluster" "devopsshack" {
-  name     = "devopsshack-cluster"
-  role_arn = aws_iam_role.devopsshack_cluster_role.arn
+resource "aws_eks_cluster" "bj" {
+  name     = "banjo-cluster"
+  role_arn = aws_iam_role.bj_cluster_role.arn
 
   vpc_config {
-    subnet_ids         = aws_subnet.devopsshack_subnet[*].id
-    security_group_ids = [aws_security_group.devopsshack_cluster_sg.id]
+    subnet_ids         = aws_subnet.banjo_subnet[*].id
+    security_group_ids = [aws_security_group.banjo_cluster_sg.id]
   }
 }
 
-resource "aws_eks_node_group" "devopsshack" {
-  cluster_name    = aws_eks_cluster.devopsshack.name
-  node_group_name = "devopsshack-node-group"
-  node_role_arn   = aws_iam_role.devopsshack_node_group_role.arn
-  subnet_ids      = aws_subnet.devopsshack_subnet[*].id
+resource "aws_eks_node_group" "banjo" {
+  cluster_name    = aws_eks_cluster.bj.name
+  node_group_name = "banjo-node-group"
+  node_role_arn   = aws_iam_role.banjo_node_group_role.arn
+  subnet_ids      = aws_subnet.banjo_subnet[*].id
 
   scaling_config {
     desired_size = 3
@@ -112,12 +112,12 @@ resource "aws_eks_node_group" "devopsshack" {
 
   remote_access {
     ec2_ssh_key = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.devopsshack_node_sg.id]
+    source_security_group_ids = [aws_security_group.banjo_node_sg.id]
   }
 }
 
-resource "aws_iam_role" "devopsshack_cluster_role" {
-  name = "devopsshack-cluster-role"
+resource "aws_iam_role" "bj_cluster_role" {
+  name = "bj-cluster-role"
 
   assume_role_policy = <<EOF
 {
@@ -135,13 +135,13 @@ resource "aws_iam_role" "devopsshack_cluster_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_cluster_role_policy" {
-  role       = aws_iam_role.devopsshack_cluster_role.name
+resource "aws_iam_role_policy_attachment" "banjo_cluster_role_policy" {
+  role       = aws_iam_role.bj_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role" "devopsshack_node_group_role" {
-  name = "devopsshack-node-group-role"
+resource "aws_iam_role" "banjo_node_group_role" {
+  name = "banjo-node-group-role"
 
   assume_role_policy = <<EOF
 {
@@ -159,17 +159,17 @@ resource "aws_iam_role" "devopsshack_node_group_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_role_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+resource "aws_iam_role_policy_attachment" "banjo_node_group_role_policy" {
+  role       = aws_iam_role.banjo_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_cni_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+resource "aws_iam_role_policy_attachment" "banjo_node_group_cni_policy" {
+  role       = aws_iam_role.banjo_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_registry_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+resource "aws_iam_role_policy_attachment" "banjo_node_group_registry_policy" {
+  role       = aws_iam_role.banjo_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
